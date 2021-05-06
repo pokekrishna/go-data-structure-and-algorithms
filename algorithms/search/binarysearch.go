c32955d6f6acfc2b0886fb89365597bd6ac009c7
krishna.gupta <krishna.gupta@go-jek.com>
2021-05-06 07:51:53 +0530

    Fix algorithm

--- algorithms/search/binarysearch.go
+++ algorithms/search/binarysearch.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 var counter int = 1
@@ -17,11 +16,17 @@ func BinarySearch(item int, list [] int) (index int){
 
 func splitList(item int, startIndex int, endIndex int, list [] int) (halvedIndex int){
 	fmt.Printf("Iteration #%d: ", counter)
-	counter ++
 
-	halvedIndex = divide(endIndex+1 - startIndex, 2) - 1
+	// break out scenario
+	if startIndex > endIndex || endIndex == 0 {
+		return -1
+	}
+
+	halvedIndex = (startIndex+endIndex)/2
+	counter ++
 	if list[halvedIndex] == item{
 		fmt.Println("Item Found at halvedIndex")
+		return
 	} else if list[halvedIndex] > item{
 		// item is on the left of the current slice
 		fmt.Printf("Item must be on the left of list. Now, Searching in index %d through %d\n",
@@ -29,27 +34,33 @@ func splitList(item int, startIndex int, endIndex int, list [] int) (halvedIndex
 		return splitList(item, startIndex, halvedIndex, list)
 	} else {
 		fmt.Printf("Item must be on the right of list. Now, Searching in index %d through %d\n",
-			halvedIndex, endIndex)
-		return splitList(item, halvedIndex, endIndex, list)
+			halvedIndex+1, endIndex)
+		return splitList(item, halvedIndex+1, endIndex, list)
 	}
-	return
-
-}
-
-func divide(numerator int, denomenator int) (answer int) {
-	n := float64(numerator)
-	d := float64(denomenator)
-
-	a := math.Ceil(n/d)
-	answer = int(a)
-	return
 }
 
 func main(){
 	list := []int {
-		1, 2, 4, 5, 10, 90, 110, 190, 210,
+		1,2, 9, 20, 31, 45, 63, 70, 100,
 	}
-	fmt.Println(list)
-	fmt.Println("index:", BinarySearch(2, list))
+
+	iterations := []int{}
+	for _,item := range list{
+		counter = 1
+		fmt.Printf("Finding %d\n---\n", item)
+		fmt.Println("index:", BinarySearch(item, list))
+		iterations = append(iterations, counter)
+		fmt.Printf("---\n\n")
+	}
+	fmt.Println("list:", list)
+	fmt.Println("Iterations: ",iterations)
+
+	counter =1
+	fmt.Printf("Finding %d\n---\n", -100)
+	fmt.Println("index:", BinarySearch(-100, list))
+
+	counter =1
+	fmt.Printf("Finding %d\n---\n", 1000)
+	fmt.Println("index:", BinarySearch(1000, list))
 
 }
