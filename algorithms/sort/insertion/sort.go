07fdf595e37bade1153b18c6acefb178c8e13e10
krishna.gupta <krishna.gupta@gojek.com>
2021-07-01 08:10:30 +0530

    Update commentary for future reference

--- algorithms/sort/insertion/sort.go
+++ algorithms/sort/insertion/sort.go
@@ -5,10 +5,12 @@ func Sort(l []int){
 		// save the item
 		elem := l[i]
 
-		// Scan from the right (index i-1 through -1) and if elem is less than item at j
-		// push forward the item by one index until elem is greater than item at j. When
-		// either scanning has reached the start of the list, or elem is greater than
-		// item, then INSERT the element and break out of j loop.
+		// Scan through the items on the left of the 'elem' - from right to left
+		// direction (index i-1 through -1). If 'elem' is less than item at j push the
+		// items by one index to the right, until 'elem' is greater than item at j, to
+		// make space for 'elem' to be inserted. When either scanning has reached the
+		// start of the list, or 'elem' is greater than item, then INSERT the element
+		// 'elem' and break out of j loop.
 		for j := i-1; j >= -1 ; j-- {
 			if j >= 0 && elem < l[j]{
 				// shift element at j by one index
